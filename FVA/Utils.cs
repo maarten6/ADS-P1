273a545badcb226aeda21eefbc815388812fa2c8
IzakAlva <f.i.haak@students.uu.nl>
2021-09-20 15:27:42 +0200

    added utils class

---
+++ FVA/Utils.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FVA
+{
+    public static class Utils
+    {
+        public static bool DEBUG = false;
+        // note that offline == !online
+        public static bool OFFLINE;
+        // processing time of the first dose
+        public static int PTIMEFIRST;
+        // processing time of the second dose
+        public static int PTIMESECOND;
+        // gap between first and second dose
+        public static int GAP;
+
+        public static void DebugPrint(string line) => Print(line, false);
+        public static void Print(string line) => Print(line, true);
+        private static void Print(string line, bool alwaysprint)
+        {
+            if (DEBUG || alwaysprint) Console.WriteLine(line);
+        }
+
+    }
+}
