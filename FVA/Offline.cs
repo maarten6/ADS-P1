3c63e3ba0bf0769f22fb1317e9027b027787a055
IzakAlva <f.i.haak@students.uu.nl>
2021-09-20 15:30:13 +0200

    setup online and offline part

---
+++ FVA/Offline.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FVA
+{
+    public class Offline
+    {
+        private int patientCNT;
+        private Patient[] patients;
+        public Offline(Patient[] patients)
+        {
+            patientCNT = patients.Length;
+            this.patients = patients;
+
+            // do scheduling here
+        }
+    }
+}
---
+++ FVA/Online.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FVA
+{
+    public class Online
+    {
+        private int patientCNT { get => patients.Count(); }
+        // not sure if this list is needed, but it will certainly be handy for debugging
+        private List<Patient> patients;
+        public Online()
+        {
+            for (string line = Console.ReadLine(); line != "x"; line = Console.ReadLine())
+            {
+                Patient cur = new Patient(line);
+                patients.Add(cur);
+                Schedule(cur);
+            }
+        }
+
+        private void Schedule(Patient patient)
+        {
+            // do scheduling here
+        }
+    }
+}
